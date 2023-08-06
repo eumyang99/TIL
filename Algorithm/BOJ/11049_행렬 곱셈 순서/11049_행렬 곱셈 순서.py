78b4bfe97435e7220232a44e4b6686146447cec9
eumyang99 <eumyang99@gmail.com>
2023-08-06 22:12:45 +0900

    230806

--- Algorithm/BOJ/11049_행렬 곱셈 순서/11049_행렬 곱셈 순서.py
+++ Algorithm/BOJ/11049_행렬 곱셈 순서/11049_행렬 곱셈 순서.py
@@ -1,62 +1,114 @@
 import sys
 input = sys.stdin.readline
 
-
-def left_search(idx):
-    l_idx = idx - 1
-    while l_idx in used and l_idx != 0:
-        l_idx -= 1
-    return l_idx
-
-def right_search(idx):
-    r_idx = idx + 1
-    while r_idx in used and r_idx != n:
-        r_idx += 1
-    return r_idx
+## 진짜 어려웠던 DP 문제
+## 풀이는 이것을 참고했음
+## https://www.youtube.com/watch?v=5MXOUix_Ud4 
 
 
 n = int(input())
-
 lst = []
-calc = [float("inf")]
-
-for i in range(n):
-    if i == 0:
-        x, y = map(int, input().split())
-        lst.append(x)
-        lst.append(y)
-    else:
-        x, y = map(int, input().split())
-        lst.append(y)
-
-for i in range(n-1):
-    calc.append(lst[i] * lst[i+1] * lst[i+2])
-else:
-    calc.append(float("inf"))
-    
-res = 0
-used = set()
+a, b = map(int, input().split())
+lst.append(a)
+lst.append(b)
 for _ in range(n-1):
-    min_num = min(calc)
-    min_idx = calc.index(min_num)
-    for i in range(n+1):
-        if calc[i] == min_num and i not in used:
-            if lst[i] > lst[min_idx]:
-                min_idx = i
-    res += min_num
-    used.add(min_idx)
-    calc[min_idx] = float("inf")
+    a, b = map(int, input().split())
+    lst.append(b)
 
-    changing_left_idx = left_search(min_idx)
-    changing_right_idx = right_search(min_idx)
 
-    if changing_left_idx != 0:
-        left_num_idx = left_search(changing_left_idx)
-        calc[changing_left_idx] = lst[left_num_idx] * lst[changing_left_idx] * lst[changing_right_idx]
+dp = [[float("inf")]*(n) for _ in range(n)]
 
-    if changing_right_idx != n:
-        right_num_idx = right_search(changing_right_idx)
-        calc[changing_right_idx] = lst[right_num_idx] * lst[changing_right_idx] * lst[changing_left_idx]
+for x in range(n):
+    a = 0
+    for y in range(n-x):
+        b = a + x
+        if a == b:
+            dp[a][b] = 0
+            a += 1
+            continue
+        mini = dp[a][b]
+        for i in range(x):
+            temp = dp[a][a+i] + dp[a+i+1][b] + lst[a-1]*lst[a+i]*lst[b]
+            if temp < mini:
+                mini = temp
+        dp[a][b] = mini 
+        a += 1
 
-print(res)
+print(dp[0][-1])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+## 틀린 방법
+## 행렬 곱셈 횟수 중 가장 작은 것들을 찾아서 먼저 계산
+## 그러나 예외 케이스를 발견 (ex 1 2 / 2 10 / 10 3 / 3 1)
+## 모범적인 풀이인 DP를 사용해서 풀기로 함
+
+# def left_search(idx):
+#     l_idx = idx - 1
+#     while l_idx in used and l_idx != 0:
+#         l_idx -= 1
+#     return l_idx
+
+# def right_search(idx):
+#     r_idx = idx + 1
+#     while r_idx in used and r_idx != n:
+#         r_idx += 1
+#     return r_idx
+
+
+# n = int(input())
+
+# lst = []
+# calc = [float("inf")]
+
+# for i in range(n):
+#     if i == 0:
+#         x, y = map(int, input().split())
+#         lst.append(x)
+#         lst.append(y)
+#     else:
+#         x, y = map(int, input().split())
+#         lst.append(y)
+
+# for i in range(n-1):
+#     calc.append(lst[i] * lst[i+1] * lst[i+2])
+# else:
+#     calc.append(float("inf"))
+    
+# res = 0
+# used = set()
+# for _ in range(n-1):
+#     min_num = min(calc)
+#     min_idx = calc.index(min_num)
+#     for i in range(n+1):
+#         if calc[i] == min_num and i not in used:
+#             if lst[i] > lst[min_idx]:
+#                 min_idx = i
+#     res += min_num
+#     used.add(min_idx)
+#     calc[min_idx] = float("inf")
+
+#     changing_left_idx = left_search(min_idx)
+#     changing_right_idx = right_search(min_idx)
+
+#     if changing_left_idx != 0:
+#         left_num_idx = left_search(changing_left_idx)
+#         calc[changing_left_idx] = lst[left_num_idx] * lst[changing_left_idx] * lst[changing_right_idx]
+
+#     if changing_right_idx != n:
+#         right_num_idx = right_search(changing_right_idx)
+#         calc[changing_right_idx] = lst[right_num_idx] * lst[changing_right_idx] * lst[changing_left_idx]
+
+# print(res)
 
--- Algorithm/BOJ/2887_행성 터널/2887_행성 터널.py
+++ Algorithm/BOJ/2887_행성 터널/2887_행성 터널.py
@@ -3,6 +3,19 @@ import sys
 input = sys.stdin.readline
 
 ## 크루스칼로 풀어봤다
+## 너무 어렵다....................
+## 이중 for문으로 모든 간선을 들고 시작하면 벌써 틀린다.
+## 사용할 간선들만 추려서 해야 한다.
+## 사용할 간선을 추리는 아이디어가 핵심이다.
+
+## x좌표를 기준으로 정렬, y좌표를 기준으로 정렬, z좌표를 기준으로 정렬한다.
+## a노드의 x가 1이고 b노드의 x가 3이고 c노드의 x가 6이면 우리는 a와 c노드의 거리를 기억할 필요가 없다.(핵심)
+## a와 b노드의 거리만 기억하면 된다.
+## 따라서 x, y, z로 정렬된 간선 정보 리스트는 바로 인접한 (i일 경우 i-1과 i+1) 노드만 거리를 재서 heap에 담는다.
+## 그러면 x, y, z 리스트에서 각각 n-1개의 간선 정보만 추릴 수 있고 heap에는 3(n-1)개만 저장된다.
+## 여기서 크루스칼을 돌린다.
+
+
 
 def find(x):
     if x == parent[x]:
@@ -23,9 +36,9 @@ for i in range(n):
     y.append((b, i))
     z.append((c, i))
 
-x.sort
-y.sort
-z.sort
+x.sort()
+y.sort()
+z.sort()
 
 pq = []
 for i in range(n-1):
@@ -35,12 +48,10 @@ for i in range(n-1):
 
 
 parent = [i for i in range(n)]
-print(pq)
 res = 0
 cnt = 0
 for i in range(n*3):
     w, s, e = heapq.heappop(pq)
-    print(w, s, e)
     ps, pe = find(s), find(e)
     if ps != pe:
         union(ps, pe)
